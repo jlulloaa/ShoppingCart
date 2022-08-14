aa2710dd3f8f50c7004eeb134562b4f31080db71
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-08-13 20:48:12 -0400

    component Cart not needed

--- cart.jsx
+++ cart.jsx
@@ -5,14 +5,6 @@ const products = [
   { name: "Beans", country: "USA", cost: 2, instock: 5 },
   { name: "Cabbage", country: "USA", cost: 1, instock: 8 },
 ];
-//=========Cart=============
-const Cart = (props) => {
-  const { Card, Accordion, Button } = ReactBootstrap;
-  let data = props.location.data ? props.location.data : products;
-  // console.log(`data:${JSON.stringify(data)}`);
-
-  return <Accordion defaultActiveKey="0">{list}</Accordion>;
-};
 
 const useDataApi = (initialUrl, initialData) => {
   const { useState, useEffect, useReducer } = React;
